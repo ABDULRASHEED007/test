9f726a7a7321f222dbd4b2d57afc18ff2d6e15cd
Abdul <mdabdulrasheed007@gmail.com>
2025-10-02 18:47:54 +0530

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hello Jhon</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
+      <h1>Hello John Cena</h1>
+     
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
